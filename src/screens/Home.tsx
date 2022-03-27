55eedc2e0a6f1438ffbb726b57a940d92fd96b7d
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-03-27 15:11:06 +0530

    setup home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,32 @@
 /** @format */
 
-import React, { FC } from "react";
-import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { FC, useState } from "react";
+import { Button, SafeAreaView, TextInput } from "react-native";
 
 const Home: FC = () => {
-	return <View></View>;
+	const [name, setName] = useState<string>("");
+
+	const navigation = useNavigation();
+
+	return (
+		<SafeAreaView testID="home">
+			<TextInput
+				placeholder="Enter country name"
+				placeholderTextColor={"grey"}
+				onChangeText={(text) => {
+					setName(text);
+				}}
+			/>
+			<Button
+				disabled={name.length == 0}
+				title={"Submit"}
+				onPress={() => {
+					navigation.navigate("List", { name: name });
+				}}
+			/>
+		</SafeAreaView>
+	);
 };
 
 export default Home;
